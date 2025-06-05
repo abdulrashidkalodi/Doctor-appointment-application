7fa379fa6a46c477c98a16897e8c04579884d8bf
abdulrashid <abdulrashidkalodi@gmail.com>
2025-06-05 10:59:14 +0530

    footer ui replaced

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -1,100 +1,144 @@
-import React from 'react'
-import {
-    MDBFooter,
-    MDBContainer,
-    MDBIcon,
-    MDBBtn
-  } from 'mdb-react-ui-kit';
+import React from "react";
+
 function Footer() {
   return (
     <div>
-          <MDBFooter className='text-center text-white' style={{ backgroundColor: '#f1f1f1' }}>
-      <MDBContainer className='pt-4'>
-        <section className='mb-4'>
-          <MDBBtn
-            rippleColor="dark"
-            color='link'
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fab fa-facebook-f' />
-          </MDBBtn>
-
-          <MDBBtn
-            rippleColor="dark"
-            color='link'
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-twitter' />
-          </MDBBtn>
-
-          <MDBBtn
-            rippleColor="dark"
-            color='link'
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-google' />
-          </MDBBtn>
-
-          <MDBBtn
-            rippleColor="dark"
-            color='link'
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-instagram' />
-          </MDBBtn>
-
-          <MDBBtn
-            rippleColor="dark"
-            color='link'
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-linkedin' />
-          </MDBBtn>
-
-          <MDBBtn
-            rippleColor="dark"
-            color='link'
-            floating
-            size="lg"
-            className='text-dark m-1'
-            href='#!'
-            role='button'
-          >
-            <MDBIcon fab className='fa-github' />
-          </MDBBtn>
-        </section>
-      </MDBContainer>
-
-      <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2023 Copyright : &nbsp;
-        <a className='text-dark' href='https://mdbootstrap.com/'>
-          docterappointment.com
-        </a>
-      </div>
-    </MDBFooter>
-
+      {/* /Contact Section */}
+      <footer id="footer" className="footer light-background">
+        <div className="container footer-top">
+          <div className="row gy-4">
+            <div className="col-lg-4 col-md-6 footer-about">
+              <a href="index.html" className="logo d-flex align-items-center">
+                <span className="sitename">Medilab</span>
+              </a>
+              <div className="footer-contact pt-3">
+                <p>A108 Adam Street</p>
+                <p>New York, NY 535022</p>
+                <p className="mt-3">
+                  <strong>Phone:</strong> <span>+1 5589 55488 55</span>
+                </p>
+                <p>
+                  <strong>Email:</strong> <span>info@example.com</span>
+                </p>
+              </div>
+              <div className="social-links d-flex mt-4">
+                <a href="">
+                  <i className="bi bi-twitter-x" />
+                </a>
+                <a href="">
+                  <i className="bi bi-facebook" />
+                </a>
+                <a href="">
+                  <i className="bi bi-instagram" />
+                </a>
+                <a href="">
+                  <i className="bi bi-linkedin" />
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-3 footer-links">
+              <h4>Useful Links</h4>
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About us</a>
+                </li>
+                <li>
+                  <a href="#">Services</a>
+                </li>
+                <li>
+                  <a href="#">Terms of service</a>
+                </li>
+                <li>
+                  <a href="#">Privacy policy</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-3 footer-links">
+              <h4>Our Services</h4>
+              <ul>
+                <li>
+                  <a href="#">Web Design</a>
+                </li>
+                <li>
+                  <a href="#">Web Development</a>
+                </li>
+                <li>
+                  <a href="#">Product Management</a>
+                </li>
+                <li>
+                  <a href="#">Marketing</a>
+                </li>
+                <li>
+                  <a href="#">Graphic Design</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-3 footer-links">
+              <h4>Hic solutasetp</h4>
+              <ul>
+                <li>
+                  <a href="#">Molestiae accusamus iure</a>
+                </li>
+                <li>
+                  <a href="#">Excepturi dignissimos</a>
+                </li>
+                <li>
+                  <a href="#">Suscipit distinctio</a>
+                </li>
+                <li>
+                  <a href="#">Dilecta</a>
+                </li>
+                <li>
+                  <a href="#">Sit quas consectetur</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-3 footer-links">
+              <h4>Nobis illum</h4>
+              <ul>
+                <li>
+                  <a href="#">Ipsam</a>
+                </li>
+                <li>
+                  <a href="#">Laudantium dolorum</a>
+                </li>
+                <li>
+                  <a href="#">Dinera</a>
+                </li>
+                <li>
+                  <a href="#">Trodelas</a>
+                </li>
+                <li>
+                  <a href="#">Flexo</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="container copyright text-center mt-4">
+          <p>
+            © <span>Copyright</span>{" "}
+            <strong className="px-1 sitename">Medilab</strong>{" "}
+            <span>All Rights Reserved</span>
+          </p>
+          <div className="credits">
+            {/* All the links in the footer should remain intact. */}
+            {/* You can delete the links only if you've purchased the pro version. */}
+            {/* Licensing information: https://bootstrapmade.com/license/ */}
+            {/* Purchase the pro version with working PHP/AJAX contact form: [buy-url] */}
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>{" "}
+            Distributed by <a href="“https://themewagon.com">ThemeWagon</a>
+          </div>
+          <a href="“https://themewagon.com"></a>
+        </div>
+        <a href="“https://themewagon.com"></a>
+      </footer>
+      <a href="“https://themewagon.com"></a>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
